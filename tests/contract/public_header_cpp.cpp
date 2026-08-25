e201926df93f796bdc6eccdba1e99668b556a992
retrodiv <retrodiv@proton.me>
2026-08-25 22:34:26 +0200

    test(api): assert the current state schema in both public-header probes

--- tests/contract/public_header_cpp.cpp
+++ tests/contract/public_header_cpp.cpp
@@ -4,7 +4,7 @@
 #include "anygm.h"
 
 static_assert(ANYGM_API_VERSION==3u,"unexpected API version");
-static_assert(ANYGM_STATE_SCHEMA==15u,"unexpected state schema");
+static_assert(ANYGM_STATE_SCHEMA==16u,"unexpected state schema");
 
 int anygm_public_header_cpp_probe(){
   AnygmHostServices services{};
