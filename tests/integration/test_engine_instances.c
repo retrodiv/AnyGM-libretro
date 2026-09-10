eebfd3cb672424e130ac188ca75a86d45da546de
retrodiv <retrodiv@proton.me>
2026-09-10 04:41:05 +0200

    test(state): pin current mouse framing and prior layouts

--- tests/integration/test_engine_instances.c
+++ tests/integration/test_engine_instances.c
@@ -3051,8 +3051,8 @@ int main(int argc,char **argv){
   write_u64(deterministic+56,state_checksum(deterministic+112,preceding_size-112));
   uint64_t preceding_hash=state_checksum(deterministic,preceding_size);
   memcpy(deterministic,first_state,first_written);
-  if(deterministic_size!=22294 ||
-     deterministic_hash!=UINT64_C(0x0be7c1443e9fa05b) ||
+  if(deterministic_size!=22298 ||
+     deterministic_hash!=UINT64_C(0x07f91baea5f9e3ff) ||
      preceding_mouse_size!=22294 || preceding_mouse_hash!=UINT64_C(0x0be7c1443e9fa05b) ||
      preceding_object_size!=22262 || preceding_object_hash!=UINT64_C(0x04e88245bcbf9fd3) ||
      preceding_size!=22070 || preceding_font_hash!=UINT64_C(0x8fe9f1b017414513) ||
